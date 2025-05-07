eb126bdb07a9deae5b4631572f592cae0f2469af
Thaumatized <veeti.junkkala@gmail.com>
2025-05-07 17:55:23 +0300

    MySQL transition

--- assignment_3/mysite/settings.py
+++ assignment_3/mysite/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database_name',
+        'USER': 'django-tutorial-migration',
+        'PASSWORD': 'opettajan-lempi-s4l4s4n4!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
